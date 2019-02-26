fa2cc9f7cb05936038c024f12e4b42b26dc54eb9
shaku237 <42364827+shaku237@users.noreply.github.com>
2019-02-26 12:24:20 +0530

    week 1 assignment

---
+++ even odd using ternary operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter an interger");
+	scanf("%d",&n);
+	
+	 if(n%2 == 0 ? printf("given number is even\n") : printf("given number is odd\n"));	
+	
+}
